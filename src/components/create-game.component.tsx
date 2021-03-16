7ff4d72a200ecf4b5dd1a73bd265c15f8bef60b6
Antony J <antony.jesudhason@verivox.com>
2021-03-16 19:45:17 +0100

    Create game component adds teams and players and used to start game.

--- src/components/create-game.component.tsx
+++ src/components/create-game.component.tsx
@@ -1,69 +1,177 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useReducer, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import './create-game.component.scss';
+import { Selector } from 'components/selector.component';
+import { GameModel } from 'models/game.model';
+import { TeamModel } from 'models/team.model';
+import { PlayerModel } from 'models/player.model';
+import { GenericKeyValuePair, ReducerAction } from 'definitions';
+import { addPlayer } from 'state-management/actions';
+import { usePlayers } from 'state-management/use-players.hook';
 
 type CreateGameProps = {
-  onGameStarted: () => void;
+  onGameStarted: (game: GameModel) => void;
 };
+
+const gameReducer = (state: GameModel, action: ReducerAction): GameModel => {
+  switch (action.type) {
+    case 'description':
+      return { ...state, description: action.payload };
+    case 'team1':
+      return { ...state, team1: teamReducer(state.team1, { type: 'name', payload: action.payload }) };
+    case 'team1Player1':
+      return { ...state, team1: teamReducer(state.team1, { type: 'player1', payload: action.payload }) };
+    case 'team1Player2':
+      return { ...state, team1: teamReducer(state.team1, { type: 'player2', payload: action.payload }) };
+    case 'team2':
+      return { ...state, team2: teamReducer(state.team2, { type: 'name', payload: action.payload }) };
+    case 'team2Player1':
+      return { ...state, team2: teamReducer(state.team2, { type: 'player1', payload: action.payload }) };
+    case 'team2Player2':
+      return { ...state, team2: teamReducer(state.team2, { type: 'player2', payload: action.payload }) };
+    default:
+      return state;
+  }
+};
+
+const teamReducer = (state: TeamModel, action: ReducerAction) => {
+  switch (action.type) {
+    case 'name':
+      return { ...state, name: action.payload };
+    case 'player1':
+      return { ...state, player1: playerReducer(state.player1, { type: 'name', payload: action.payload }) };
+    case 'player2':
+      return { ...state, player2: playerReducer(state.player1, { type: 'name', payload: action.payload }) };
+    default:
+      return state;
+  }
+};
+
+const playerReducer = (state: PlayerModel, action: ReducerAction) => {
+  switch (action.type) {
+    case 'name':
+      return { ...state, name: action.payload };
+    default:
+      return state;
+  }
+};
+
+const initialTeams = ['Real Madrid', 'FC Bayern', 'Hertha BSC'];
 
 export const CreateGame: FunctionComponent<CreateGameProps> = (props: CreateGameProps) => {
   const { onGameStarted } = props;
+  const [state, updateGame] = useReducer(gameReducer, new GameModel());
+  const [teams, setTeams] = useState(initialTeams);
+  const [errorMessage, setErrorMessage] = useState('');
+  const reduxDispatch = useDispatch();
+  const objPlayers: GenericKeyValuePair = usePlayers();
+  const players: string[] = Object.keys(objPlayers);
 
-  const handleSubmit = () => {
-    console.log('game created');
-    onGameStarted();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    if (
+      state.description === '' ||
+      state.team1.name === '' ||
+      state.team2.name === '' ||
+      state.team1.player1.name === '' ||
+      state.team2.player1.name === ''
+    ) {
+      setErrorMessage('Enter description and at least one player per team');
+      return;
+    }
+    onGameStarted(state);
   };
 
-  const onValueChange = (event: React.FormEvent<HTMLInputElement | HTMLSelectElement>): void => {
+  const onDescriptionChange = (event: React.FormEvent<HTMLInputElement>): void => {
     const eventTarget = event.currentTarget;
-    console.log(eventTarget.value);
+    onItemChange(eventTarget.name, eventTarget.value);
+  };
+
+  const onItemChange = (name: string, value: string) => {
+    updateGame({ type: name, payload: value });
+    setErrorMessage('');
+  };
+
+  const onNewTeam = (name: string, value: string) => {
+    setTeams([...teams, value]);
+    onItemChange(name, value);
+  };
+
+  const onNewPlayer = (name: string, value: string) => {
+    const newPlayer = new PlayerModel(value);
+    reduxDispatch(addPlayer(newPlayer));
+    onItemChange(name, value);
   };
 
   return (
     <form className="start-game" onSubmit={handleSubmit}>
       <label className="start-game__description">
         Game description
-        <input type="text" className="edit" minLength={1} maxLength={20} onChange={onValueChange} />
+        <input
+          type="text"
+          className="edit"
+          minLength={1}
+          maxLength={20}
+          name="description"
+          value={state.description}
+          onChange={onDescriptionChange}
+        />
       </label>
       <fieldset className="start-game__team1">
         <legend>Team 1</legend>
-        <label>
-          Name
-          <select className="select" onChange={onValueChange}>
-            <option>T1</option>
-            <option>T2</option>
-            <option>T3</option>
-          </select>
-          <input type="button" className="button" value="+" />
-        </label>
-        <label>
-          Player 1
-          <select className="select" onChange={onValueChange} />
-          <input type="button" className="button" value="+" />
-        </label>
-        <label>
-          Player 2
-          <select className="select" onChange={onValueChange} />
-          <input type="button" className="button" value="+" />
-        </label>
+        <Selector
+          onChange={onItemChange}
+          onNew={onNewTeam}
+          options={teams}
+          label="Team"
+          name="team1"
+          value={state.team1.name}
+        />
+        <Selector
+          label="Player 1"
+          name="team1Player1"
+          options={players}
+          value={state.team1.player1.name}
+          onChange={onItemChange}
+          onNew={onNewPlayer}
+        />
+        <Selector
+          label="Player 2"
+          name="team1Player2"
+          options={players}
+          value={state.team1.player2.name}
+          onChange={onItemChange}
+          onNew={onNewPlayer}
+        />
       </fieldset>
       <fieldset className="start-game__team2">
         <legend>Team 2</legend>
-        <label>
-          Name
-          <select className="select" onChange={onValueChange} />
-          <input type="button" className="button" value="+" />
-        </label>
-        <label>
-          Player 1
-          <select className="select" onChange={onValueChange} />
-          <input type="button" className="button" value="+" />
-        </label>
-        <label>
-          Player 2
-          <select className="select" onChange={onValueChange} />
-          <input type="button" className="button" value="+" />
-        </label>
+        <Selector
+          onChange={onItemChange}
+          onNew={onNewTeam}
+          options={teams}
+          label="Team"
+          name="team2"
+          value={state.team2.name}
+        />
+        <Selector
+          label="Player 1"
+          name="team2Player1"
+          options={players}
+          value={state.team2.player1.name}
+          onChange={onItemChange}
+          onNew={onNewPlayer}
+        />
+        <Selector
+          label="Player 2"
+          name="team2Player2"
+          options={players}
+          value={state.team2.player2.name}
+          onChange={onItemChange}
+          onNew={onNewPlayer}
+        />
       </fieldset>
+      {errorMessage !== '' && <p className="error start-game__error-message">{errorMessage}</p>}
       <div className="start-game__cta">
         <input type="submit" className="button button--primary" value="Start Game" />
       </div>
