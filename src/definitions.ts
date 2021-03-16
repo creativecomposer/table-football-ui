30615053975b67c2563eba3d21bb03b425517e5c
Antony J <antony.jesudhason@verivox.com>
2021-03-16 18:50:01 +0100

    Add definitions file for common type definitions.

---
+++ src/definitions.ts
@@ -0,0 +1,30 @@
+import { Dispatch, AnyAction } from 'redux';
+
+export type GameScore = {
+  team1: {
+    player1: number;
+    player2: number;
+  };
+  team2: {
+    player1: number;
+    player2: number;
+  };
+};
+
+export type GenericKeyValuePair = {
+  [key: string]: any;
+};
+
+export type ReducerAction = {
+  type: string;
+  payload: any;
+};
+
+export type ReduxPropsType = {
+  dispatch: Dispatch<AnyAction>;
+};
+
+export type RootState = {
+  games: any;
+  players: GenericKeyValuePair;
+};
