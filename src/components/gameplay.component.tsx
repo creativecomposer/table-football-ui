0228d75991bd1878165067964f1725a4489d2d8d
Antony J <antony.jesudhason@verivox.com>
2021-03-16 19:47:03 +0100

    Gameplay component records goals and submits results to games component

--- src/components/gameplay.component.tsx
+++ src/components/gameplay.component.tsx
@@ -1,16 +1,55 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useReducer } from 'react';
 import './gameplay.component.scss';
+import { GameModel } from 'models/game.model';
+import { GameScore } from 'definitions';
 
 type GameplayProps = {
-  onFinish: () => void;
+  data: GameModel;
+  onFinish: (score: GameScore) => void;
   onDiscard: () => void;
 };
 
-export const Gameplay: FunctionComponent<GameplayProps> = (props: GameplayProps) => {
-  const { onFinish, onDiscard } = props;
+type ReducerAction = {
+  type: string;
+  payload: any;
+};
 
-  const handleSubmit = () => {
-    onFinish();
+const initialScore: GameScore = {
+  team1: {
+    player1: 0,
+    player2: 0,
+  },
+  team2: {
+    player1: 0,
+    player2: 0,
+  },
+};
+
+const scoreReducer = (state: any, action: ReducerAction) => {
+  switch (action.type) {
+    case 'team1Player1':
+      return { ...state, team1: { ...state.team1, player1: action.payload } };
+    case 'team1Player2':
+      return { ...state, team1: { ...state.team1, player2: action.payload } };
+    case 'team2Player1':
+      return { ...state, team2: { ...state.team2, player1: action.payload } };
+    case 'team2Player2':
+      return { ...state, team2: { ...state.team2, player2: action.payload } };
+  }
+};
+
+export const Gameplay: FunctionComponent<GameplayProps> = (props: GameplayProps) => {
+  const { data, onFinish, onDiscard } = props;
+  const [score, dispatch] = useReducer(scoreReducer, initialScore);
+
+  const onInputChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    const eventTarget = event.currentTarget;
+    dispatch({ type: eventTarget.name, payload: parseInt(eventTarget.value) });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    onFinish(score);
   };
 
   const handleDiscard = () => {
@@ -20,31 +59,69 @@ export const Gameplay: FunctionComponent<GameplayProps> = (props: GameplayProps)
   return (
     <form className="gameplay" onSubmit={handleSubmit}>
       <div className="gameplay__title">
-        <span>Team 1 vs Team 2</span>
+        <span>
+          {data?.team1.name} vs {data?.team2.name}
+        </span>
       </div>
       <fieldset className="gameplay__team1">
-        <legend>Team 1</legend>
+        <legend>{data?.team1.name}</legend>
         <label>
-          Player 1
-          <input type="number" min="0" max="10" className="edit" />
+          {data?.team1.player1.name}
+          <input
+            type="number"
+            min="0"
+            max="10"
+            name="team1Player1"
+            onChange={onInputChange}
+            value={score.team1.player1}
+            className="edit gameplay__goal"
+          />
         </label>
-        <label>
-          Player 2
-          <input type="number" min="0" max="10" className="edit" />
-        </label>
-        <label>Goal Total 0</label>
+        {data?.team1.player2.name && (
+          <label>
+            {data?.team1.player2.name}
+            <input
+              type="number"
+              min="0"
+              max="10"
+              name="team1Player2"
+              onChange={onInputChange}
+              value={score.team1.player2}
+              className="edit gameplay__goal"
+            />
+          </label>
+        )}
+        <label>Goal Total {score.team1.player1 + score.team1.player2}</label>
       </fieldset>
       <fieldset className="gameplay__team2">
-        <legend>Team 2</legend>
+        <legend>{data?.team2.name}</legend>
         <label>
-          Player 1
-          <input type="number" min="0" max="10" className="edit" />
+          {data?.team2.player1.name}
+          <input
+            type="number"
+            min="0"
+            max="10"
+            name="team2Player1"
+            onChange={onInputChange}
+            value={score.team2.player1}
+            className="edit gameplay__goal"
+          />
         </label>
-        <label>
-          Player 2
-          <input type="number" min="0" max="10" className="edit" />
-        </label>
-        <label>Goal Total 0</label>
+        {data?.team2.player2.name && (
+          <label>
+            {data?.team2.player2.name}
+            <input
+              type="number"
+              min="0"
+              max="10"
+              name="team2Player2"
+              onChange={onInputChange}
+              value={score.team2.player2}
+              className="edit gameplay__goal"
+            />
+          </label>
+        )}
+        <label>Goal Total {score.team2.player1 + score.team2.player2}</label>
       </fieldset>
       <div className="gameplay__cta">
         <input type="submit" className="button button--primary" value="Finish & Save Game*" />
