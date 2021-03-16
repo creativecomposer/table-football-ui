54bca3ca00708bbca5f2dfbe51b4736af39ce485
Antony J <antony.jesudhason@verivox.com>
2021-03-16 22:42:50 +0100

    Add heading and TBD text for player page

--- src/components/players.component.tsx
+++ src/components/players.component.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export function Players() {
-  return <h2>Players</h2>;
+  return (
+    <div className="page-wrapper">
+      <h2 className="page-title">Game Log for a Player</h2>
+      <h3>TBD</h3>
+    </div>
+  );
 }
