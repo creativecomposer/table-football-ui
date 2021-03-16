fa1b7425f4ecdf38d643175ced16aad08d472a9c
Antony J <antony.jesudhason@verivox.com>
2021-03-16 18:52:34 +0100

    Update App component test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createReduxStore } from 'state-management/store';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const store = createReduxStore();
+
+test('should render app header', () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const headerElement = screen.getByText(/Table Football Score Tracker/i);
+  expect(headerElement).toBeInTheDocument();
 });
