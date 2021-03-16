070ab9dbce834e6b945afc22be8e3a08531a60ce
Antony J <antony.jesudhason@verivox.com>
2021-03-16 21:12:54 +0100

    Games component to dispatch game info to redux store

--- src/components/games.component.tsx
+++ src/components/games.component.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
+import React, { FunctionComponent, useState } from 'react';
+import { Prompt } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import './games.component.scss';
 import { CreateGame } from 'components/create-game.component';
 import { Gameplay } from 'components/gameplay.component';
+import { GameModel } from 'models/game.model';
+import { GameScore } from 'definitions';
+import { tallyResults } from 'business-logic/score-calculation';
+import { addGame } from 'state-management/actions';
 
-export function Games() {
+export const Games: FunctionComponent = () => {
   const [gameStarted, setGameStarted] = useState(false);
+  const [gameData, setGameData] = useState(new GameModel());
+  const reduxDispatch = useDispatch();
 
-  const handleGameStart = () => {
+  const handleGameStart = (game: GameModel) => {
+    setGameData(game);
     setGameStarted(true);
   };
 
-  const handleGameEnd = () => {
+  const handleGameDiscard = () => {
+    setGameStarted(false);
+  };
+
+  const handleGameFinish = (score: GameScore): void => {
+    addGame(reduxDispatch, tallyResults(gameData, score));
     setGameStarted(false);
   };
 
@@ -18,10 +32,11 @@ export function Games() {
     <div className="page-wrapper">
       <h2 className="page-title">Start / Create a Game</h2>
       {gameStarted ? (
-        <Gameplay onDiscard={handleGameEnd} onFinish={handleGameEnd} />
+        <Gameplay data={gameData} onDiscard={handleGameDiscard} onFinish={handleGameFinish} />
       ) : (
         <CreateGame onGameStarted={handleGameStart} />
       )}
+      <Prompt when={gameStarted} message="Game will be discarded. Do you want to continue?" />
     </div>
   );
-}
+};
