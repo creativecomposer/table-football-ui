39231ad9c6b620bce89ba9b5b4aa81dac48cd984
Antony J <antony.jesudhason@verivox.com>
2021-03-29 21:24:14 +0200

    Fetch player list on app startup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { HashRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { AppRoutes } from './Route';
 import { Navigation } from 'components/navigation.component';
+import { ErrorBoundary } from 'components/error-boundary.component';
 import './App.scss';
+import { fetchPlayers } from 'state-management/actions';
 
 function App() {
+  const reduxDispatch = useDispatch();
+  useEffect(() => fetchPlayers(reduxDispatch), []);
+
   return (
-    <>
+    <ErrorBoundary>
       <header className="App-header">
         <p>The Table Football Score Tracker</p>
       </header>
@@ -17,7 +23,7 @@ function App() {
         </main>
       </HashRouter>
       <footer></footer>
-    </>
+    </ErrorBoundary>
   );
 }
 
